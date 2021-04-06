01aad23284df9e553b13751a15aa4932687ff9b0
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-04-05 23:06:31 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer
+
+function setBestCustomer() {
+    // var bestCustomer;
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(name) {
+    bestCustomer = name;
+}
+
+const leastFavoriteCustomer = "Sick Boy";
+
+function changeLeastFavoriteCustomer(name) {
+    leastFavoriteCustomer = name;
+}
